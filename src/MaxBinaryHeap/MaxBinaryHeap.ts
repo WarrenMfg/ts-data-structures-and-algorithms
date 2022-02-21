c0674b83d6cb063a781b729a7247f45ae9fa2cae
WarrenMfg <warrenmfg@icloud.com>
2022-02-21 06:58:58 -0500

    adds max binary heap

---
+++ src/MaxBinaryHeap/MaxBinaryHeap.ts
@@ -0,0 +1,58 @@
+export class MaxBinaryHeap {
+  _values: number[];
+
+  constructor() {
+    this._values = [];
+  }
+
+  insert(value: number) {
+    let newValueIndex = this._values.push(value) - 1;
+    let parentIndex: number | undefined, temp: number;
+
+    // bubble up
+    while (parentIndex !== 0) {
+      parentIndex = Math.floor((newValueIndex - 1) / 2);
+      if (this._values[parentIndex] < this._values[newValueIndex]) {
+        temp = this._values[parentIndex];
+        this._values[parentIndex] = this._values[newValueIndex];
+        this._values[newValueIndex] = temp;
+        newValueIndex = parentIndex;
+      } else break;
+    }
+
+    return this;
+  }
+
+  extractMax() {
+    const last = this._values.pop();
+    if (!this._values.length) return last;
+
+    const max = this._values[0];
+    this._values[0] = last as number;
+    let parent = 0,
+      left: number | undefined,
+      right: number | undefined,
+      largestChild: number | undefined,
+      temp: number | undefined;
+
+    // sink down
+    while (true) {
+      left = 2 * parent + 1;
+      right = left + 1;
+
+      largestChild = this._values[right] > this._values[left] ? right : left;
+
+      if (this._values[largestChild] > this._values[parent]) {
+        temp = this._values[parent];
+        this._values[parent] = this._values[largestChild];
+        this._values[largestChild] = temp;
+
+        parent = largestChild;
+      } else {
+        break;
+      }
+    }
+
+    return max;
+  }
+}
